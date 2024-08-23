4fb28d0197156aa75e56c42d09a74eb5154a5818
LauraP <166152671+LauraPeckyno@users.noreply.github.com>
2024-08-23 11:37:59 -0400

    building some addtl modules to work with the income results. I may do something to indicate the higher of the reseults (maybe a green backgournd?). I also adjust some of the styling

--- validate.mjs
+++ validate.mjs
@@ -3,7 +3,7 @@ export {validateZipCodes};
 // added my original validation back into this function
 
 const validateZipCodes = (zipCode1, zipCode2) => {
-    if (!zipCode1 || !zipCode2 || zipCode1.length !== 5 || zipCode2.length !== 5 || !/^\d+$/.test(zipCode)) {   // making sure the zips are right. Is it falsy, does it have more than 5 characters. does it have characters other than numbers
+    if (!zipCode1 || !zipCode2 || zipCode1.length !== 5 || zipCode2.length !== 5 || !/^\d+$/.test(zipCode1) || !/^\d+$/.test(zipCode2)) {   // making sure the zips are right. Is it falsy, does it have more than 5 characters. does it have characters other than numbers
       return false;
     }
     return true;
